a15a03a5f1fbe14d3b492767aab029b9c0a2ac4f
luck73 <91341865+luck73@users.noreply.github.com>
2022-03-25 10:39:19 -0300

    Add files via upload

---
+++ src/app/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+{
+  path:'', pathMatch: 'full', redirectTo: 'busca-reativa'
+},
+
+{
+  path: 'busca-reativa',
+  loadChildren: () => import('./reactive-search/reactive-search.module').then(m => m.ReactiveSearchModule)
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
